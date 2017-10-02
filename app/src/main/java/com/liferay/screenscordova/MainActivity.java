d431d9de7b225bc0251ae2ef912df884133d5426
Victor Galan <victor.galan@liferay.com>
2017-10-02 09:59:54 +0200

    MyBank update

---
+++ app/src/main/java/com/liferay/screenscordova/MainActivity.java
@@ -0,0 +1,12 @@
+
+package com.liferay.screenscordova;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
